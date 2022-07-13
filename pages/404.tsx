0a2f3f53f95a96a06716e3191c68c42f2e7e561e
rajatcbd <rm2932002@gmail.com>
2022-07-13 10:43:08 +0530

    :recycle: refactor(setup): custom 404 and 500 pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,9 @@
-export default function Custom500() {
+export default function Custom404() {
   return (
     <div className='server_crashed_page'>
       {/*  eslint-disable-next-line @next/next/no-img-element */}
-      <img src='/assets/500.svg' alt='500' />
-      <h1>500 - Internal server error</h1>
+      <img src='/assets/404.svg' alt='500' />
+      <h1>404 - Page not found</h1>
     </div>
   );
 }
