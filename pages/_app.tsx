7cce313c1584958ba2022f2b107e0a0e6222b02c
rajatcbd <rm2932002@gmail.com>
2022-07-13 14:31:56 +0530

    :white_check_mark: test(setup): 500 error test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,18 @@ import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import supabase from '../utils/supabase';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  supabase.auth.onAuthStateChange((event, session) => {
+    console.log('This is Executing');
+    fetch('/api/auth', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      credentials: 'same-origin',
+      body: JSON.stringify({ event, session }),
+    });
+  });
   return (
     <>
       <NextNProgress color='#f1c40f' />
